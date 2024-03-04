63f455a2bf708b58549dbdd3cf9c562d137aac9a
Erick Fleming <efleming969@gmail.com>
2024-03-04 11:51:16 -0500

    feat: remove complicated build

--- release.config.js
+++ release.config.js
@@ -2,46 +2,14 @@
  * @type {import("semantic-release").GlobalConfig}
  */
 module.exports = {
-    branches: ['+([0-9])?(.{+([0-9]),x}).x', 'trunk', { name: 'rc', prerelease: true }],
+    branches: [ "trunk" ],
     repositoryUrl: "https://github.com/efleming969/semiver-v1",
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        '@semantic-release/changelog',
-        [
-            'semantic-release-replace-plugin',
-            {
-                replacements: [
-                    {
-                        files: ['package.json'],
-                        from: '"version": ".*"',
-                        to: '"version": "${nextRelease.version}"',
-                    },
-                    {
-                        files: [
-                            'package.json'
-                        ],
-                        from: '"version": ".*"',
-                        to: '"version": "${nextRelease.version}"',
-                    },
-                ],
-            },
-        ],
+        "@semantic-release/changelog",
         "@semantic-release/npm",
-        [
-            '@semantic-release/git',
-            {
-                message: 'chore(changelog): ${nextRelease.version}',
-                assets: [
-                    'CHANGELOG.md',
-                    'package.json',
-                ],
-            },
-        ],
-        [
-        '@semantic-release/exec', {
-                publishCmd: 'npm publish',
-            },
-        ],
+        "@semantic-release/git",
+        "@semantic-release/github"
     ]
 }
